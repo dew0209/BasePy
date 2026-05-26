b26ed028def680ae0fb9e19ff44d5cfde9a5eaab
jsyx <ly020922@aliyun.com>
2026-05-26 19:29:33 +0800

    [langChain概述]

---
+++ langchain/01_langchain介绍.py
@@ -0,0 +1,14 @@
+"""
+langChain学习的困难
+    1.知识体系庞杂
+    2.langChain更新速度很酷啊
+    3.官方的文档，资料不够完善
+
+1.langChain使用概述
+2.langChain使用之Model IO
+3.langChain使用之Chains
+4.langChain使用之Memory
+5.langChain使用之Tools
+6.langChain使用之Agents
+7.langChain使用之Retrieval
+"""
